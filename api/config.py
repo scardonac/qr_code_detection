52bd4ec5d73ff90d1483f56fd28d3e5713795f0e
Sara Cardona C. <cardonasara571@gmail.com>
2024-09-10 16:35:32 -0500

    Agregando modelo entrenado

--- api/config.py
+++ api/config.py
@@ -1,4 +1,4 @@
 import os
 
 # Configuración de rutas y parámetros globales
-MODEL_PATH = os.path.join("runs", "detect", "qr_code_detection2", "weights", "best.pt")
+MODEL_PATH = os.path.join("models", "best_model", "best.pt")
